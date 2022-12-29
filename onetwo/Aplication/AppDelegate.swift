d630b029038be50efb00916c9f16df6393346918
Aleksandr Provarenko <provar67@gmail.com>
2022-12-29 12:53:37 +0300

    Added App icon and chenged LaunchScreen

--- onetwo/Aplication/AppDelegate.swift
+++ onetwo/Aplication/AppDelegate.swift
@@ -6,15 +6,12 @@
 //
 
 import UIKit
-import FirebaseCore
-
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        FirebaseApp.configure()
+    
         return true
     }
 
